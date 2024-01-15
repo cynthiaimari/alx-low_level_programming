a539fb1611bcbf6c42bfaa284075caef75c6c62e
cynthiaimari <cynthiaimari@gmail.com>
2024-01-15 11:37:03 -0800

    Hacking\

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprintf.so -fPIC test.c
-export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
+wget -p .. https://raw.githubusercontent.com/cynthiaimari/alx-low_level_programming/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
